298035e53ec535d5e51827affa519cd4d7b4e19d
Lorat1403 <lorat1403@gmail.com>
2025-08-25 19:07:31 +0300

    add some examples

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Test} from './components/test/test/test';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Test],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('angular-using-examples');
+
+  messageFromParent: string = 'Я твій батько';
+  messageFromChild: string = '';
+newTask: any;
+
+  getMessageFromChild(message: string) {
+    this.messageFromChild = message;
+  }
+  
 }
