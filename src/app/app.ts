35026a7c8ec6b858cef4e0565d695a4aee7cb69b
Lorat1403 <lorat1403@gmail.com>
2025-11-06 13:30:10 +0200

    edit deployment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Test } from './components/test/test';
-import {
-  TestForm
-} from './components/test-form/test-form';
+import { TestForm } from './components/test-form/test-form';
 import { RxjsTest } from './components/rxjs-test/rxjs-test';
 import { RxjsSubject } from './components/rxjs-subject/rxjs-subject';
 import { RxjsOperators } from './components/rxjs-operators/rxjs-operators';
